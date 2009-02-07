4f43675fb37ecc29e6673e6ed73be1adb88c07da
lindqvust <ay.buenavista@gmail.com>
2009-02-07 11:36:04 +0800

    Pulled changes from jonasespelita integrate branch

--- oasisapp/db/migrate/20090202013101_add_fk_changes.rb
+++
@@ -1,8 +0,0 @@
-class AddFkChanges < ActiveRecord::Migration
-  def self.up
-  	execute "ALTER TABLE changes ADD CONSTRAINT fk_changes_to_admin FOREIGN KEY (admin_id) REFERENCES admins (id);"
-  end
-
-  def self.down
-  end
-end
